9ea7d48e460e61040a7e892d4fe8a89903952356
Leonardo Miyagi <lmiyagi@jera.com.br>
2017-06-28 21:47:01 -0400

    creating appointments by api request

--- app/controllers/api/v1/users/appointments_controller.rb
+++ app/controllers/api/v1/users/appointments_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::Users::AppointmentsController < Api::V1::ApiController
   before_action :require_authentication
 
+  def create
+    @appointment = current_user.appointments.create(date: Time.parse(appointment_params[:date]))
+    if @appointment.save
+      respond_with @appointment, location: ''
+    else
+      render_unprocessable_error
+    end
+  end
+
   def show
     @appointment = current_user.appointments.find_by(id: params[:id])
     if @appointment.present?
@@ -8,5 +17,11 @@ class Api::V1::Users::AppointmentsController < Api::V1::ApiController
     else
       render_not_found_error
     end
+  end
+
+  private
+
+  def appointment_params
+    params.permit(:date)
   end
 end
